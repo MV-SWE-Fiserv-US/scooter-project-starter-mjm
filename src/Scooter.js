f39216b8cdd590b60e7bc6b6b43c0b20fd3ec96d
Melissa Yefsah <melissa.yefsah@melissa.yefsah-MacBook-Pro>
2024-11-01 11:30:41 -0400

    Added Scooter class and it's tets

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,30 @@
-class Scooter{
-  // scooter code here
+class Scooter {
+  // Scooter code here
+  static nextSerial=1;
+  constructor(station) {
+    this.station=station;
+    this.user=null;
+    this.serial = Scooter.nextSerial++;
+    this.charge=100;
+    this.isBroken=false;
+    
+  }
+  rent(user) {
+    if (this.charge > 20 && !this.isBroken) {
+      this.station = null;
+      this.user = user;//Return the user that rent the scooter
+      return user; 
+    } else if (this.charge <= 20) {
+      throw new Error('scooter needs to charge');
+    } else if (this.isBroken) {
+      throw new Error('scooter needs repair');
+    }
+  }
+  dock(station){
+    this.station = station ;
+    this.user = null;
+  }
+
 }
 
-
-module.exports = Scooter
+module.exports = Scooter;
